41a01e57dfbc1b9728b9905016e361b889b07781
l37sg0 <petar_1v@abv.bg>
2023-04-29 11:13:49 +0300

    [Footer]  - some work on the footer.  - made sections visible.  - sections now link to diff menu navigators.

--- footer.php
+++ footer.php
@@ -3,35 +3,28 @@
     <footer class="py-5">
         <div class="row">
             <div class="col-6 col-md-2 mb-3">
-                <h5>Section</h5>
                 <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+                    <li class="nav-item mb-2"><a href="<?php echo home_url(); ?>"
+                                                 class="nav-link p-0 text-light">Home</a></li>
+                    <li class="nav-item mb-2"><a href="<?php echo home_url(); ?>/#about"
+                                                 class="nav-link p-0 text-light">About</a></li>
+                    <li class="nav-item mb-2"><a href="<?php echo home_url(); ?>/#services"
+                                                 class="nav-link p-0 text-light">Services</a></li>
+                    <li class="nav-item mb-2"><a href="<?php echo home_url(); ?>/#contact"
+                                                 class="nav-link p-0 text-light">Contact</a></li>
                 </ul>
             </div>
 
             <div class="col-6 col-md-2 mb-3">
-                <h5>Section</h5>
                 <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
-                </ul>
-            </div>
-
-            <div class="col-6 col-md-2 mb-3">
-                <h5>Section</h5>
-                <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+                    <li class="nav-item mb-2"><a href="<?php echo home_url(); ?>/#tech-stack"
+                                                 class="nav-link p-0 text-light">TechStack</a></li>
+                    <li class="nav-item mb-2"><a href="<?php echo home_url(); ?>/#api-experience"
+                                                 class="nav-link p-0 text-light">API Experience</a></li>
+                    <li class="nav-item mb-2"><a href="<?php echo home_url(); ?>/#projects"
+                                                 class="nav-link p-0 text-light">Projects</a></li>
+                    <li class="nav-item mb-2"><a href="<?php echo home_url(); ?>/blog"
+                                                 class="nav-link p-0 text-light">Blog</a></li>
                 </ul>
             </div>
 
@@ -49,37 +42,27 @@
         </div>
 
         <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-            <p>&copy; 2022 Company, Inc. All rights reserved.</p>
+            <p>2021-<?php echo date('Y'); ?> <a class="text-light" href="https://l37sg0.com">l37sg0.com</a></p>
             <div class="container p-4">
                 <!-- Section: Social media -->
                 <section class="mb-4">
                     <!-- Facebook -->
-                    <a class="btn btn-outline-dark btn-floating m-1" href="#!" role="button"
+                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
                     ><i class="fab fa-facebook-f"></i
                         ></a>
 
-                    <!-- Twitter -->
-                    <a class="btn btn-outline-dark btn-floating m-1" href="#!" role="button"
-                    ><i class="fab fa-twitter"></i
-                        ></a>
-
-                    <!-- Google -->
-                    <a class="btn btn-outline-dark btn-floating m-1" href="#!" role="button"
-                    ><i class="fab fa-google"></i
-                        ></a>
-
                     <!-- Instagram -->
-                    <a class="btn btn-outline-dark btn-floating m-1" href="#!" role="button"
-                    ><i class="fab fa-instagram"></i
+                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                    ><i class="fab fa-youtube"></i
                         ></a>
 
                     <!-- Linkedin -->
-                    <a class="btn btn-outline-dark btn-floating m-1" href="#!" role="button"
+                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
                     ><i class="fab fa-linkedin-in"></i
                         ></a>
 
                     <!-- Github -->
-                    <a class="btn btn-outline-dark btn-floating m-1" href="#!" role="button"
+                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
                     ><i class="fab fa-github"></i
                         ></a>
                 </section>
