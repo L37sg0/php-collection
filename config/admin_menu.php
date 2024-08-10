265f769ddf9d6b94807bdf97dcc54f63e4ebae8b
l37sg0 <petar_1v@abv.bg>
2024-08-10 15:20:25 +0300

    Add config for admin_menu - #9

---
+++ config/admin_menu.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'Dashboard' => [
+//        'icon' => '<svg class="bi"><use xlink:href="#house-fill"/></svg >',
+        'icon' => '<i class="fa fa-dashboard"></i>',
+        'route' => 'admin.dashboard'
+    ]
+];
---
+++ local-src/l37sg0/badmin/config/admin_menu.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'Dashboard' => [
+        'icon' => '<svg class="bi"><use xlink:href="#house-fill"/></svg >',
+        'route' => 'admin.dashboard'
+    ]
+];
