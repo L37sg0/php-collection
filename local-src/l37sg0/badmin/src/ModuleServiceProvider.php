590b8d50c12dce2e5ca84a3277d18cdeec19d5fd
l37sg0 <petar_1v@abv.bg>
2024-08-10 15:19:43 +0300

    Register admin_menu config - #9

--- local-src/l37sg0/badmin/src/ModuleServiceProvider.php
+++ local-src/l37sg0/badmin/src/ModuleServiceProvider.php
@@ -8,11 +8,20 @@ use L37sg0\Badmin\Controller\DashboardController;
 
 class ModuleServiceProvider extends ServiceProvider
 {
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/admin_menu.php' => config_path('admin_menu.php')
+        ], 'config');
+    }
+
     public function register()
     {
         Route::group(['as' => 'admin.', 'prefix' => 'admin'], function (){
             Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+            Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
         });
         $this->loadViewsFrom(__DIR__ . '/../views', 'admin');
+        $this->mergeConfigFrom(__DIR__ . '/../config/admin_menu.php', config_path('admin_menu.php'));
     }
 }
