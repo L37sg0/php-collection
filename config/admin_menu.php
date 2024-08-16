a009b26a69b2438d8cd23dec87566076641fca77
l37sg0 <petar_1v@abv.bg>
2024-08-16 21:21:54 +0300

    Empty config admin_menu, so can be merged on the go - #9

--- config/admin_menu.php
+++ config/admin_menu.php
@@ -1,9 +1,5 @@
 <?php
 
 return [
-    'Dashboard' => [
-//        'icon' => '<svg class="bi"><use xlink:href="#house-fill"/></svg >',
-        'icon' => '<i class="fa fa-dashboard"></i>',
-        'route' => 'admin.dashboard'
-    ]
+
 ];
