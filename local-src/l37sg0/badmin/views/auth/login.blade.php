527a8d107b21bc7f5a5fd942dd2b46a48224faea
l37sg0 <petar_1v@abv.bg>
2024-08-31 13:41:09 +0300

    Refactor login and register templates - #9

--- local-src/l37sg0/badmin/views/auth/login.blade.php
+++ local-src/l37sg0/badmin/views/auth/login.blade.php
@@ -7,23 +7,30 @@
 
 @section('page_body')
     @parent
-    <div class="container-fluid">
-        <div class="row">
-            <main class="form-signin w-100 m-auto">
+    <div class="container d-flex justify-content-center align-items-center vh-100">
+        <div class="card" style="width: 100%; max-width: 600px;">
+            <div class="card-body">
+                <h2 class="card-title text-left text-success">
+                    {{ trans('Please Login') }}
+                </h2>
                 <form method="POST" action="{{ route('login') }}">
                     @csrf
-                    {{--                <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">--}}
-                    <h1 class="h3 mb-3 fw-normal">{{trans('Please log in')}}</h1>
 
                     <div class="form-floating">
-                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"
+                        <input type="email" class="form-control mb-3" id="floatingInput" placeholder="name@example.com"
                                name="email">
                         <label for="floatingInput">{{trans('Email address')}}</label>
+                        @error('email')
+                        <div class="text-danger">{{ $message }}</div>
+                        @enderror
                     </div>
                     <div class="form-floating">
-                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password"
+                        <input type="password" class="form-control mb-3" id="floatingPassword" placeholder="Password"
                                name="password">
                         <label for="floatingPassword">{{trans('Password')}}</label>
+                        @error('password')
+                        <div class="text-danger">{{ $message }}</div>
+                        @enderror
                     </div>
 
                     <div class="form-check text-start my-3">
@@ -32,11 +39,16 @@
                             {{trans('Remember me')}}
                         </label>
                     </div>
-                    <button class="btn btn-primary w-100 py-2" type="submit">{{trans('Log in')}}</button>
+                    <button class="btn btn-outline-success w-100 py-2" type="submit">{{trans('Log in')}}</button>
                 </form>
-                    <a class="btn btn-link" href="{{route('register')}}">{{ trans('Don\'t have an account? Register') }}</a>
-            </main>
+                <div class="card-footer">
+                    <a class="btn btn-link"
+                       href="{{route('register')}}">{{ trans('Don\'t have an account? Register') }}</a>
 
+                    <a class="btn btn-link"
+                       href="{{route('password.request')}}">{{ trans('I\'ve forgot my password') }}</a>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
--- local-src/l37sg0/badmin/views/auth/register.blade.php
+++ local-src/l37sg0/badmin/views/auth/register.blade.php
@@ -7,41 +7,54 @@
 
 @section('page_body')
     @parent
-    <div class="container-fluid">
-        <div class="row">
-            <main class="form-signin w-100 m-auto">
+    <div class="container d-flex justify-content-center align-items-center vh-100">
+        <div class="card" style="width: 100%; max-width: 600px;">
+            <div class="card-body">
+                <h2 class="card-title text-left text-success">
+                    {{ trans('Register') }}
+                </h2>
                 <form method="POST" action="{{ route('register') }}">
                     @csrf
-                    {{--                <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">--}}
-                    <h1 class="h3 mb-3 fw-normal">{{ trans('Register') }}</h1>
 
                     <div class="form-floating">
-                        <input required type="text" class="form-control" id="floatingInput" placeholder="Your Name"
-                               name="name">
+                        <input required type="text" class="form-control mb-3" id="floatingInput" placeholder="Your Name"
+                               name="name" value="{{ old('name', '') }}">
                         <label for="floatingInput">{{ trans('Name') }}</label>
+                        @error('name')
+                        <div class="text-danger">{{ $message }}</div>
+                        @enderror
                     </div>
 
                     <div class="form-floating">
-                        <input required type="email" class="form-control" id="floatingInput"
-                               placeholder="name@example.com" name="email">
+                        <input required type="email" class="form-control mb-3" id="floatingInput"
+                               placeholder="name@example.com" name="email" value="{{ old('email', '') }}">
                         <label for="floatingInput">{{trans('Email address')}}</label>
+                        @error('email')
+                        <div class="text-danger">{{ $message }}</div>
+                        @enderror
                     </div>
 
                     <div class="form-floating">
-                        <input required type="password" class="form-control" id="floatingPassword"
-                               placeholder="Password" name="password">
+                        <input required type="password" class="form-control mb-3" id="floatingPassword"
+                               placeholder="Password" name="password" value="{{ old('password', '') }}">
                         <label for="floatingPassword">{{trans('Password')}}</label>
+                        @error('password')
+                        <div class="text-danger">{{ $message }}</div>
+                        @enderror
                     </div>
 
                     <div class="form-floating">
-                        <input required type="password" class="form-control" id="floatingPassword"
+                        <input required type="password" class="form-control mb-3" id="floatingPassword"
                                placeholder="Password"
-                               name="password_confirmation">
+                               name="password_confirmation" value="{{ old('password_confirmation', '') }}">
                         <label for="floatingPassword">{{trans('Confirm Password')}}</label>
+                        @error('password_confirmation')
+                        <div class="text-danger">{{ $message }}</div>
+                        @enderror
                     </div>
-                    <button class="btn btn-primary w-100 py-2" type="submit">{{trans('Register')}}</button>
+                    <button class="btn btn-outline-success w-100 py-2" type="submit">{{trans('Register')}}</button>
                 </form>
-            </main>
+            </div>
         </div>
     </div>
 @endsection
