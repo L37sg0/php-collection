2b471dc189f8ea2172bb78e5175e167c074ddade
l37sg0 <petar_1v@abv.bg>
2024-10-03 07:08:20 +0300

    Register translation service provider in core - #12

--- local-src/l37sg0/core/src/ModuleServiceProvider.php
+++ local-src/l37sg0/core/src/ModuleServiceProvider.php
@@ -2,10 +2,17 @@
 
 namespace L37sg0\Core;
 
+use Illuminate\Translation\TranslationServiceProvider;
 use L37sg0\Core\Providers\CoreServiceProvider;
 
 class ModuleServiceProvider extends CoreServiceProvider
 {
+    public function register()
+    {
+        parent::register();
+        $this->app->register(TranslationServiceProvider::class);
+    }
+
     public function boot()
     {
         $this->publishes([
