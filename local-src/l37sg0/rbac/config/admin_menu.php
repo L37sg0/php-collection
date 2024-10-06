d8bf9e328199b6b04a7ff23b6f7585ce9d76f36c
l37sg0 <petar_1v@abv.bg>
2024-10-06 11:13:15 +0300

    Apply access via permissions on module, and add command for syncing perimissions into database - #1

--- local-src/l37sg0/rbac/config/admin_menu.php
+++ local-src/l37sg0/rbac/config/admin_menu.php
@@ -5,17 +5,18 @@ return [
         'icon' => '<i class="fa fa-id-badge"></i>',
         'route' => null,
         'order' => 10,
-        'can' => 'manage_accounts',
         'children' => [
             'Roles' => [
                 'icon' => '<i class="fa fa-id-card"></i>',
                 'route' => 'admin.roles.list',
                 'order' => 10,
+                'can' => 'manage_roles',
             ],
             'Users' => [
                 'icon' => '<i class="fa fa-users"></i>',
                 'route' => 'admin.users.list',
                 'order' => 11,
+                'can' => 'manage_users',
             ]
         ]
     ],
