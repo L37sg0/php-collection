7d010ebe92bf7f27f1796b1e5dbcfbe6958c3436
l37sg0 <petar_1v@abv.bg>
2025-02-22 22:35:33 +0200

    Add tables and model for tags - #1

---
+++ app/Models/Tag.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+/**
+ * @property int    id
+ * @property string created_at
+ * @property string updated_at
+ * @property string name
+ * @property string slug
+ */
+class Tag extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug'
+    ];
+
+    public function news(): BelongsToMany
+    {
+        return $this->belongsToMany(News::class, 'news_tags');
+    }
+}
