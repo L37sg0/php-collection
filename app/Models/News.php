c12902848d5ec5c396d701b2c07f1ac5f2b0d085
l37sg0 <petar_1v@abv.bg>
2025-02-22 21:58:30 +0200

    Add event listener to post news on facebook when created - #1

--- app/Models/News.php
+++ app/Models/News.php
@@ -2,14 +2,40 @@
 
 namespace App\Models;
 
+use App\Services\FacebookService;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property string title
+ * @property string slug
+ * @property string content
+ * @property string image
+ * @property int    category_id
+ */
 class News extends Model
 {
+    protected $fillable = [
+        'title',
+        'slug',
+        'content',
+        'image',
+        'category_id',
+    ];
+
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    protected static function booted(): void
+    {
+        static::created(function ($news) {
+            $facebook = new FacebookService();
+            $message = "📢 Нова статия: {$news->title}";
+            $link = url('/news/' . $news->slug);
+            $facebook->postToPage($message, $link);
+        });
     }
 
 }
