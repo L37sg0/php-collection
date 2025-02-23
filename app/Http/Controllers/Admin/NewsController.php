8c75afbe79af398c000b1c2a8f6ff8fba2c38045
l37sg0 <petar_1v@abv.bg>
2025-02-23 15:34:38 +0200

    Implement logic in News admin controller - #1

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -2,7 +2,55 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\News;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
 class NewsController
 {
+    public function index()
+    {
+        $news = News::all();
+        return view('admin.news.index', compact('news'));
+    }
 
+    public function edit()
+    {
+        $newsItem = News::find(request()->query('id'));
+        return view('admin.news.edit', compact('newsItem'));
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        if (!isset($data['slug']) || empty($data['slug'])) {
+            $data = array_merge($data, [
+                'slug' => Str::slug($data['name'])
+            ]);
+        }
+
+        $newsItem = News::create($data);
+        return response()->redirectToRoute('admin.news.list')->with('success', trans('News Item saved successfully!'));
+    }
+
+    public function update(Request $request)
+    {
+        $data = $request->all();
+        if (!isset($data['slug']) || empty($data['slug'])) {
+            $data = array_merge($data, [
+                'slug' => Str::slug($data['name'])
+            ]);
+        }
+
+        $newsItem = News::find($data['id']);
+        $newsItem->update($data);
+
+        return response()->redirectToRoute('admin.news.list')->with('success', trans('News Item updated successfully!'));
+    }
+
+    public function destroy()
+    {
+        News::find(request()->query('id'))->delete();
+        return response()->redirectToRoute('admin.news.list')->with('info', trans('News Item deleted successfully!'));
+    }
 }
