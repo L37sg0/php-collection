9fa6b9f6fb951488ab9930c5737d14c8ab881fa8
l37sg0 <petar_1v@abv.bg>
2025-02-23 16:35:13 +0200

    Add templates for edit and listing in admin news - #1

---
+++ resources/views/admin/news/index.blade.php
@@ -0,0 +1,53 @@
+@php use App\Models\News;use Illuminate\Support\Str; @endphp
+@extends('admin::admin.admin')
+
+
+@section('content_header')
+    @parent
+    <div
+        class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">{{trans('News')}}</h1>
+        <div class="btn-toolbar mb-2 mb-md-0">
+            <div class="btn-group me-2">
+                <a href="{{ route('admin.news.edit') }}"
+                   class="btn btn-sm btn-outline-success">{{trans('New NewsItem')}}</a>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+@section('content_body')
+    @parent
+    <div class="table-responsive small">
+        <table class="table table-striped table-sm">
+            <thead>
+            <tr>
+                <th scope="col">{{ trans('#') }}</th>
+                <th scope="col">{{ trans('Title') }}</th>
+                <th scope="col">{{ trans('Slug') }}</th>
+                <th scope="col">{{ trans('Content') }}</th>
+                <th scope="col">{{ trans('Actions') }}</th>
+            </tr>
+            </thead>
+            <tbody>
+            @php /** @var News $newsItem */ @endphp
+            @foreach($news as $newsItem)
+                <tr>
+                    <td>{{ $newsItem->id }}</td>
+                    <td>{{ $newsItem->title }}</td>
+                    <td>{{ $newsItem->slug }}</td>
+                    <td>{!! Str::limit(strip_tags($newsItem->content), 50) !!}</td>
+                    <td>
+                        <a href="{{ route('admin.news.edit', ['id' => $newsItem->id]) }}"
+                           class="btn btn-sm btn-outline-success" id="edit"><i class="fas fa-pencil"></i></a>
+                        <a href="{{ route('admin.news.delete', ['id' => $newsItem->id]) }}"
+                           class="btn btn-sm btn-outline-danger" id="delete"><i class="fas fa-trash-can"></i></a>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+
+@endsection
